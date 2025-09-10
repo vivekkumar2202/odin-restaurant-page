6ae786635ed9b3da394fb163c8e6e657639b8072
Vivek <137711336+vivekkumar2202@users.noreply.github.com>
2025-09-10 06:20:05 +0000

    split webpack.config.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,12 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { watch, watchFile } = require("fs");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
